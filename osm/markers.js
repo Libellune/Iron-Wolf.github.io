b6bcdaeb07f1c09792955c19cf001beba875b89d
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2021-08-13 01:07:00 +0200

    Update markers.js

--- osm/markers.js
+++ osm/markers.js
@@ -2,7 +2,7 @@ var markers = [
   {
     name: 'Black Temple Food',
     url: 'https://blacktemplefood.fr/menu/',
-    lat:  48,1143392,
-    lng: -1,6816653,
+    lat:  48.1143392,
+    lng: -1.6816653,
   }
 ]
