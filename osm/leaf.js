46ffadadc938a44e66c782652abe979c6ba04c3f
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2021-08-13 00:57:16 +0200

    Update leaf.js

--- osm/leaf.js
+++ osm/leaf.js
@@ -8,3 +8,23 @@ L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: ['a','b','c']
 }).addTo( map );
+
+// https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png
+var myIcon = L.icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14],
+})
+
+for (var i = 0; i < markers.length; ++i) {
+  L.marker([markers[i].lat, markers[i].lng], { icon: myIcon })
+    .bindPopup(
+      '<a href="' +
+        markers[i].url +
+        '" target="_blank">' +
+        markers[i].name +
+        '</a>'
+    )
+    .addTo(map)
+}
