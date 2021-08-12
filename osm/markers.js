1a752969c1bb263bd0507a6d3530b240d7aac8d8
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2021-08-13 01:21:57 +0200

    Update markers.js

--- osm/markers.js
+++ osm/markers.js
@@ -4,5 +4,11 @@ var markers = [
     url: 'https://blacktemplefood.fr/menu/',
     lat:  48.1143392,
     lng: -1.6816653,
+  },
+  {
+    name: 'Les Brocanteurs',
+    url: 'https://lesbrocanteursrennes.com/lacarte/',
+    lat: 48.1110520, 
+    lng: -1.6809059,
   }
 ]
