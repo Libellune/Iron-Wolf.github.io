fed38ebff64f1d23ada069094e9e7bc94b82e89a
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2021-08-15 23:56:26 +0200

    Update markers.js

--- osm/markers.js
+++ osm/markers.js
@@ -1,14 +1,74 @@
 var markers = [
   {
-    name: 'Black Temple Food',
+    name: 'Black Temple Food 🧆',
     url: 'https://blacktemplefood.fr/menu/',
     lat:  48.1143392,
     lng: -1.6816653,
   },
   {
-    name: 'Les Brocanteurs',
+    name: 'Les Brocanteurs 🍔',
     url: 'https://lesbrocanteursrennes.com/lacarte/',
     lat: 48.1110520, 
     lng: -1.6809059,
-  }
+  },
+  {
+    name: 'La Gavotte 🥞',
+    url: 'https://www.creperie-rennes.fr/',
+    lat: 48.1119433, 
+    lng: -1.6754210,
+  },
+  {
+    name: 'Le Fuji 🍜',
+    url: 'https://lesbrocanteursrennes.com/lacarte/',
+    lat: 48.1113398, 
+    lng: -1.6765000,
+  },
+  {
+    name: 'Ouzh-Taol 🥞',
+    url: 'https://www.creperie-rennes.fr/',
+    lat: 48.1149012,
+    lng: -1.6763218,
+  },
+  {
+    name: 'L\'Épi de Blé 🥞',
+    url: 'https://www.creperie-rennes.fr/',
+    lat: 48.1092346,
+    lng: -1.6794212,
+  },
+  {
+    name: 'Avec 🍔',
+    url: 'http://avec-rennes.com/',
+    lat: 48.0964392,
+    lng: -1.6346714,
+  },
+  {
+    name: 'Les Trois Brasseurs 🍻',
+    url: 'https://restaurants.3brasseurs.com/fr/3-brasseurs-rennes',
+    lat: 48.13184,
+    lng: -1.68879,
+  },
+  {
+    name: 'Le Haricot Rouge 🍫',
+    url: 'https://le-haricot-rouge-rennes.business.site/',
+    lat: 48.1105183,
+    lng: -1.6785702,
+  },
+  {
+    name: 'Poutinebros 🍟',
+    url: 'https://www.poutinebros.com/',
+    lat: 48.1138143,
+    lng: -1.6809343,
+  },
+  {
+    name: 'Ker Soazig 🥞',
+    url: 'https://www.kersoazig.fr',
+    lat: 48.1138143,
+    lng: -1.6809343,
+  },
+  {
+    name: 'Le Fournil Vasselot 🍪',
+    url: 'https://www.kersoazig.fr',
+    lat: 48.1087099,
+    lng: -1.6784183,
+  },
 ]
